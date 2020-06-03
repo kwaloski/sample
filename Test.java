f9319e107ac784dd2d57714f46721ded351eb0ab
kwaloski <shubhamdeolal@gmail.com>
2020-06-04 01:38:59 +0530

    second commit

--- Test.java
+++ Test.java
@@ -2,7 +2,7 @@ public class Test{
 	
 			public static void main(String[] args){
 		/**
-		@wi.LinkRoleAsASingleWord SHUB-553:100 Hello 
+		@wi.LinkRoleAsASingleWord AD-552:100 Hello 
 		**/
 	
 		System.out.println("Hello world");
