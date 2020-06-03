e407f303de55dbbaee02f14e294888ad3558c9ad
kwaloski <shubhamdeolal@gmail.com>
2020-06-03 23:42:08 +0530

    First Commit

---
+++ Test.java
@@ -0,0 +1,10 @@
+public class Test{
+	
+			public static void main(String[] args){
+		/**
+		@wi.LinkRoleAsASingleWord SHUB-553:100 Hello 
+		**/
+	
+		System.out.println("Hello world");
+								}
+}
