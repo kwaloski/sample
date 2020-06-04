867c9ec37ad568070a06804511c6f5430861e4ea
kwaloski <shubhamdeolal@gmail.com>
2020-06-04 16:54:16 +0530

    Forth Commit

--- Test.java
+++ Test.java
@@ -1,9 +1,9 @@
 public class Test{
-	
-			public static void main(String[] args){
 		/**
 		 * @wi.LinkRoleAsASingleWord AD-552:200 Hello 
 		 */
+	
+			public static void main(String[] args){
 	
 		System.out.println("Hello world");
 								}
