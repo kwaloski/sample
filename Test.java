9e348df5ca45db19181d832ac1f313015796f426
kwaloski <40489017+kwaloski@users.noreply.github.com>
2020-06-09 21:11:56 +0530

    Delete Test.java

--- Test.java
+++
@@ -1,9 +0,0 @@
-public class Test{
-		/**
-		 * @wi.LinkRoleAsASingleWord AD-552 This is demo 
-		 */
-			public static void main(String[] args){
-	
-		System.out.println("Hello worlds");
-								}
-}
